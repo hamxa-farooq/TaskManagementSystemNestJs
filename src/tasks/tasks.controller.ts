2b3155526b8ed7fac575d65624f475dbde2b8274
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-02-17 15:32:11 +0500

    Completed CRUD operations for Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { Task } from './task.model';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateTaskDto, TasksFilterDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -7,7 +17,34 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() tasksFilterDto: TasksFilterDto): Task[] {
+    if (Object.keys(tasksFilterDto).length) {
+      return this.tasksService.getTasksWithFilter(tasksFilterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('taskStatus') taskStatus: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, taskStatus);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.tasksService.deleteTaskByid(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
-
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto, TasksFilterDto } from './dto/create-task.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -10,7 +10,27 @@ export class TasksService {
     return this.tasks;
   }
 
-  public createTask(title: string, description: string): Task {
+  public getTasksWithFilter(tasksFilterDto: TasksFilterDto): Task[] {
+    const { status, search } = tasksFilterDto;
+    let tasks = this.getAllTasks();
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+    return tasks;
+  }
+
+  public getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task = {
       id: uuid(),
       title,
@@ -19,5 +39,15 @@ export class TasksService {
     };
     this.tasks.push(task);
     return task;
+  }
+
+  updateTaskStatus(id: string, taskStatus: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = taskStatus;
+    return task;
+  }
+
+  public deleteTaskByid(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
